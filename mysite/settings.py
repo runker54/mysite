58992cdf1122dfd0989c1a2e3e9b8f59ab34f6b6
Runker54 <59903664+runker54@users.noreply.github.com>
2022-11-23 14:39:13 +0800

    Update settings.py
    删除秘钥

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^fd2p$yt-0aru(pp=!vmnh3y$42o(w^=3)4*s5nzjlxl)#bdx%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
